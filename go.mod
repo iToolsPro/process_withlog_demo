93c3baf01ccb1550ffb1782670bd44da7c4be8b3
jinxufang <jinxufang@tencent.cmo>
2020-12-06 16:36:57 +0800

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module mygolog
+
+go 1.15
+
+require (
+	github.com/imfht/progressbar v1.0.0 // indirect
+	github.com/schollz/progressbar/v3 v3.7.2
+)
+
+replace github.com/schollz/progressbar/v3 v3.7.2 => github.com/iToolsPro/progressbar/v3 v3.7.6
---
+++ run.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/schollz/progressbar/v3"
+	"time"
+)
+
+func main() {
+	bar := progressbar.NewOptions(100,
+		progressbar.OptionEnableColorCodes(true),
+		progressbar.OptionSetWidth(60),
+		progressbar.OptionSetDescription("logWriter demo"),
+		progressbar.OptionSetTheme(progressbar.Theme{
+			Saucer:        "[green]=[reset]",
+			SaucerHead:    "[green]>[reset]",
+			SaucerPadding: " ",
+			BarStart:      "[",
+			BarEnd:        "]",
+		}))
+	for i := 0; i < 100; i++ {
+		bar.Add(1)
+		bar.WriteLog(fmt.Sprintf("%s sample log ---> %d", time.Now().Format("2006-01-02 15:04:05"), i))
+		time.Sleep(time.Second)
+	}
+}
